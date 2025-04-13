ba76fb30db23edc98beb84d57afeb46f58e048c7
pabloN <pablonicholas2012@gmail.com>
2025-04-13 13:01:06 -0300

    refact(script, index): criei lógica para capturar data e hora direto da API.

--- script/script.js
+++ script/script.js
@@ -23,3 +23,69 @@ document.addEventListener('DOMContentLoaded', function () {
 
   setInterval(changeImage, 4500)
 })
+
+
+fetch('https://integracao.cardapioweb.com/api/menu/company/profile?company=bolos_di_sofi&hostname=app.oxemenu.com.br')
+  .then(response => response.json())
+  .catch(error => console.error(error))
+
+  
+  const diasSemanaPT = {
+    sunday: "Domingo",
+    monday: "Segunda-feira",
+    tuesday: "Terça-feira",
+    wednesday: "Quarta-feira",
+    thursday: "Quinta-feira",
+    friday: "Sexta-feira",
+    saturday: "Sábado"
+  }
+
+  const diaDaSemanaHoje = new Date().toLocaleDateString('en-US', { weekday: 'long' }).toLowerCase()
+
+  async function getBusinessHours() {
+    try {
+      const response = await fetch('https://integracao.cardapioweb.com/api/menu/company/profile?company=bolos_di_sofi&hostname=app.oxemenu.com.br')
+      const data = await response.json()
+      const businessHours = data.business_hours
+
+      const hojeHorario = businessHours[diaDaSemanaHoje]
+
+      const container = document.getElementById('horario-funcionamento')
+      const agendamento = document.getElementById('agendamento')
+      if (hojeHorario && hojeHorario.length > 0) {
+        const [abertura, fechamento] = hojeHorario[0]
+        const dataAgora = new Date()
+        const horaAgora = dataAgora.getHours() + (dataAgora.getMinutes() / 60)
+        if (horaAgora < parseInt(abertura.split(':')[0])) {
+          agendamento.innerHTML = `<p>Disponivel para agendamento</p>`
+        }}
+      if (hojeHorario && hojeHorario.length > 0) {
+        const [abertura, fechamento] = hojeHorario[0]
+        container.innerHTML = `<p>Hoje ${diasSemanaPT[diaDaSemanaHoje]} Aberto das: ${abertura} às ${fechamento}</p>`
+      } 
+     
+        else {
+        
+        const dias = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+        let diaAtualIndex = dias.indexOf(diaDaSemanaHoje)
+        let proximoDia, horarios
+
+        for (let i = 1; i <= 7; i++) {
+          const index = (diaAtualIndex + i) % 7
+          const dia = dias[index]
+          if (businessHours[dia] && businessHours[dia].length > 0) {
+            proximoDia = dia
+            horarios = businessHours[dia][0]
+            break
+          }
+        }
+
+        const [aberturaProxima] = horarios
+        container.innerHTML = `<p>Apenas agendamento: Abriremos ${diasSemanaPT[proximoDia]} às ${aberturaProxima}</p>`
+      }
+    } catch (error) {
+      console.error('Erro ao buscar horários:', error)
+    }
+  }
+
+  getBusinessHours()
